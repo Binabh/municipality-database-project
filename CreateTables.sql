6643871b138863401a8e5c2e7196f8a76e4d91e2
Binabh <binabhdevkota@gmail.com>
2021-12-25 13:31:39 +0545

    driver program changed

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Wards(
+CREATE TABLE IF NOT EXISTS Wards(
     ward_id INTEGER UNIQUE PRIMARY KEY,
     ward_name VARCHAR(70),
     location_lat FLOAT(6),
     location_lon FLOAT(6)
 );
 
-CREATE TABLE Households(
+CREATE TABLE IF NOT EXISTS Households(
     house_id INTEGER UNIQUE,
     ward_id INTEGER,
     owner_name VARCHAR(70),
--- driverprogram.py
+++ driverprogram.py
@@ -3,22 +3,39 @@ import psycopg2
 conn = psycopg2.connect(database = "municipality_db", user = "postgres", password = "postgres", host = "postgres_container", port = "5432")
 print("Opened database successfully")
 cur = conn.cursor()
+def quit_or_continue():
+    ask = input("[q]uit or [c]ontinue ? :")
+    if(ask.lower() == "q"):
+        cur.close()
+        conn.close()
+        return
+    elif(ask.lower() == "c"):
+        main()
+    else:
+        quit_or_continue()
 
 def create_table():
     cur.execute(open("CreateTables.sql", "r").read())
+    conn.commit()
     print("Tables created successfully!")
+    quit_or_continue()
 
 def delete_table():
     cur.execute(open("DeleteTables.sql", "r").read())
+    conn.commit()
     print("Tables deleted successfully!")
+    quit_or_continue()
 
 def insert_data():
-    # TODO: ADD SQL CODE
+    cur.execute(open("InsertData.sql", "r").read())
+    conn.commit()
     print("Data inserted successfully!")
+    quit_or_continue()
 
 def query_data():
     # TODO: ADD SQL CODE
     print("Data queried successfully!")
+    quit_or_continue()
 
 def main():
     choice = input("What you want to do?\n1. Create tables\n2. Populate Tables\n3. Drop tables\n4. Query Data\n5. Exit\n")
@@ -31,6 +48,8 @@ def main():
     elif(choice == "4"):
         query_data()
     elif(choice == "5"):
+        cur.close()
+        conn.close()
         return
     else:
         print("Invalid choice! Please re-enter.")
